2f931371b2a2417cc0858f8185fd47c134c11205
euberdeveloper <euberdeveloper>
2020-03-14 19:59:20 +0100

    docs: added doc comments

--- source/index.ts
+++ source/index.ts
@@ -7,7 +7,15 @@ import { getCodes } from './utils/getCodes';
 import { transpile } from './utils/transpile';
 import getGenerators from './generators';
 
+export { Options } from './types';
 
+/**
+ * Fetches all the template files in the given folder (files whose extension is preceded
+ * by .template) and generate the code inside the special comments (such as //{{COMMENT}})
+ * @param src The folder where the template files will be fetched from. The default is the current folder.
+ * @param structure The json file containing the structure, used by generators to dynamically generate code. The default is structure.json.
+ * @param options The options specifying things such as logging, indentation and filters on the files
+ */
 export function generate(src?: string, structure?: string, options?: Options) {
     src = src || process.cwd();
     structure = structure || path.join(process.cwd(), 'structure.json');
@@ -18,5 +26,3 @@ export function generate(src?: string, structure?: string, options?: Options) {
     const codes = getCodes(structure, generators);
     transpile(src, codes, options, logger);
 }
-
-generate('./shit');
