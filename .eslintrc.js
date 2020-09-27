aee2309c94fd75f99ea6c3d814f65065bbe7a18f
euberdeveloper <euberdeveloper@gmail.com>
2020-09-27 15:35:40 +0200

    style(.eslimtrc.js): fix winzoz

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,9 @@ module.exports = {
     },
     rules: {
         'no-empty-function': 'off',
+        '@typescript-eslint/no-extra-semi': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }]
